e341fa21535f504fd6d17d4383d1cb4465181482
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-24 19:08:02 -0800

    Move to readthedocs.

--- README.md
+++ README.md
@@ -1,32 +1,17 @@
 Indic transliteration tools
 =======================
 [![Build Status](https://travis-ci.org/sanskrit-coders/indic_transliteration.svg?branch=master)](https://travis-ci.org/sanskrit-coders/indic_transliteration)
+[![Documentation Status](https://readthedocs.org/projects/indic-transliteration/badge/?version=latest)](http://indic-transliteration.readthedocs.io/en/latest/?badge=latest)
 
 
 # For users
 For detailed examples and help, please see individual module files in this package.
 
 ## Installation or upgrade:
-* `sudo pip2 install indic_transliteration -U`
-* `sudo pip2 install git+https://github.com/sanskrit-coders/indic_transliteration/@master -U`
+* `sudo pip install indic_transliteration -U`
+* `sudo pip install git+https://github.com/sanskrit-coders/indic_transliteration/@master -U`
 * [Web](https://pypi.python.org/pypi/indic-transliteration).
-
-## Transliteration
-```
-from indic_transliteration import sanscript
-output = sanscript.transliterate('idam adbhutam', sanscript.HK, sanscript.DEVANAGARI)
-sanscript.transliterate(u"गच्छ",sanscript.DEVANAGARI, sanscript.HK)
-```
-
-Scripts supported: devanagari tamil malayalam telugu gurumukhi gujarati bengali oriya  Harvard-Kyoto IAST (aka Roman-Unicode) SLP1 WX
-
-## Script detection
-`detect.py` automatically detects a string's transliteration scheme:
-```
-from indic_transliteration import detect
-detect.detect('pitRRIn') == Scheme.ITRANS
-detect.detect('pitRRn') == Scheme.HK
-```
+* [Docs](http://indic-transliteration.readthedocs.io/en/latest/).
 
 # For contributors
 ## Contact
--- indic_transliteration/detect.py
+++ indic_transliteration/detect.py
@@ -6,6 +6,14 @@
     Code for automatically detecting a transliteration scheme.
 
     :license: MIT and BSD
+
+Example usage:
+
+::
+
+    from indic_transliteration import detect
+    detect.detect('pitRRIn') == Scheme.ITRANS
+    detect.detect('pitRRn') == Scheme.HK
 """
 
 import re
@@ -14,21 +22,21 @@ import re
 import sys
 
 SCHEMES = [
-    ('Bengali', 0x0980),
-    ('Devanagari', 0x0900),
-    ('Gujarati', 0x0a80),
-    ('Gurmukhi', 0x0a00),
-    ('Kannada', 0x0c80),
-    ('Malayalam', 0x0d00),
-    ('Oriya', 0x0b00),
-    ('Tamil', 0x0b80),
-    ('Telugu', 0x0c00),
-    ('HK', None),
-    ('IAST', None),
-    ('ITRANS', None),
-    ('Kolkata', None),
-    ('SLP1', None),
-    ('Velthuis', None),
+  ('Bengali', 0x0980),
+  ('Devanagari', 0x0900),
+  ('Gujarati', 0x0a80),
+  ('Gurmukhi', 0x0a00),
+  ('Kannada', 0x0c80),
+  ('Malayalam', 0x0d00),
+  ('Oriya', 0x0b00),
+  ('Tamil', 0x0b80),
+  ('Telugu', 0x0c00),
+  ('HK', None),
+  ('IAST', None),
+  ('ITRANS', None),
+  ('Kolkata', None),
+  ('SLP1', None),
+  ('Velthuis', None),
 ]
 
 #: Start of the Devanagari block.
@@ -41,71 +49,70 @@ BRAHMIC_LAST_CODE_POINT = 0x0d7f
 BLOCKS = sorted([x for x in SCHEMES if x[-1]], key=lambda x: -x[1])
 
 #: Enum for Sanskrit schemes.
-Scheme = type('Enum', (), {name : name for name, code in SCHEMES})
+Scheme = type('Enum', (), {name: name for name, code in SCHEMES})
 
 
 class Regex:
+  #: Match on special Roman characters
+  IAST_OR_KOLKATA_ONLY = re.compile(u'[āīūṛṝḷḹēōṃḥṅñṭḍṇśṣḻ]')
 
-    #: Match on special Roman characters
-    IAST_OR_KOLKATA_ONLY = re.compile(u'[āīūṛṝḷḹēōṃḥṅñṭḍṇśṣḻ]')
+  #: Match on chars shared by ITRANS and Velthuis
+  ITRANS_OR_VELTHUIS_ONLY = re.compile(u'aa|ii|uu|~n')
 
-    #: Match on chars shared by ITRANS and Velthuis
-    ITRANS_OR_VELTHUIS_ONLY = re.compile(u'aa|ii|uu|~n')
+  #: Match on ITRANS-only
+  ITRANS_ONLY = re.compile(u'ee|oo|\^[iI]|RR[iI]|L[iI]|' \
+                           u'~N|N\^|Ch|chh|JN|sh|Sh|\\.a')
 
-    #: Match on ITRANS-only
-    ITRANS_ONLY = re.compile(u'ee|oo|\^[iI]|RR[iI]|L[iI]|' \
-                             u'~N|N\^|Ch|chh|JN|sh|Sh|\\.a')
+  #: Match on Kolkata-specific Roman characters
+  KOLKATA_ONLY = re.compile(u'[ēō]')
 
-    #: Match on Kolkata-specific Roman characters
-    KOLKATA_ONLY = re.compile(u'[ēō]')
+  #: Match on SLP1-only characters and bigrams
+  SLP1_ONLY = re.compile(u'[fFxXEOCYwWqQPB]|kz|Nk|Ng|tT|dD|Sc|Sn|' \
+                         u'[aAiIuUfFxXeEoO]R|' \
+                         u'G[yr]|(\\W|^)G')
 
-    #: Match on SLP1-only characters and bigrams
-    SLP1_ONLY = re.compile(u'[fFxXEOCYwWqQPB]|kz|Nk|Ng|tT|dD|Sc|Sn|' \
-                           u'[aAiIuUfFxXeEoO]R|' \
-                           u'G[yr]|(\\W|^)G')
-
-    #: Match on Velthuis-only characters
-    VELTHUIS_ONLY = re.compile(u'\\.[mhnrlntds]|"n|~s')
+  #: Match on Velthuis-only characters
+  VELTHUIS_ONLY = re.compile(u'\\.[mhnrlntds]|"n|~s')
 
 
 def detect(text):
-    """Detect the input's transliteration scheme.
+  """Detect the input's transliteration scheme.
 
     :param text: some text data, either a `unicode` or a `str` encoded
                  in UTF-8.
     """
-    if sys.version_info < (3, 0):
-      # Verify encoding
-      try:
-          text = text.decode('utf-8')
-      except UnicodeError:
-          pass
+  if sys.version_info < (3, 0):
+    # Verify encoding
+    try:
+      text = text.decode('utf-8')
+    except UnicodeError:
+      pass
 
-    # Brahmic schemes are all within a specific range of code points.
-    for L in text:
-        code = ord(L)
-        if code >= BRAHMIC_FIRST_CODE_POINT:
-            for name, start_code in BLOCKS:
-                if start_code <= code <= BRAHMIC_LAST_CODE_POINT:
-                    return name
+  # Brahmic schemes are all within a specific range of code points.
+  for L in text:
+    code = ord(L)
+    if code >= BRAHMIC_FIRST_CODE_POINT:
+      for name, start_code in BLOCKS:
+        if start_code <= code <= BRAHMIC_LAST_CODE_POINT:
+          return name
 
-    # Romanizations
-    if Regex.IAST_OR_KOLKATA_ONLY.search(text):
-        if Regex.KOLKATA_ONLY.search(text):
-            return Scheme.Kolkata
-        else:
-            return Scheme.IAST
+  # Romanizations
+  if Regex.IAST_OR_KOLKATA_ONLY.search(text):
+    if Regex.KOLKATA_ONLY.search(text):
+      return Scheme.Kolkata
+    else:
+      return Scheme.IAST
 
-    if Regex.ITRANS_ONLY.search(text):
-        return Scheme.ITRANS
+  if Regex.ITRANS_ONLY.search(text):
+    return Scheme.ITRANS
 
-    if Regex.SLP1_ONLY.search(text):
-        return Scheme.SLP1
+  if Regex.SLP1_ONLY.search(text):
+    return Scheme.SLP1
 
-    if Regex.VELTHUIS_ONLY.search(text):
-        return Scheme.Velthuis
+  if Regex.VELTHUIS_ONLY.search(text):
+    return Scheme.Velthuis
 
-    if Regex.ITRANS_OR_VELTHUIS_ONLY.search(text):
-        return Scheme.ITRANS
+  if Regex.ITRANS_OR_VELTHUIS_ONLY.search(text):
+    return Scheme.ITRANS
 
-    return Scheme.HK
+  return Scheme.HK

7d681b6b7fff6555eeef2b6a94beb6684d73ea30
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-26 17:02:17 -0800

    Fix tests = 1 fails

--- tests/deduplication_test.py
+++ tests/deduplication_test.py
@@ -20,7 +20,14 @@ logging.info(test_data["duplicates"])
 
 
 @pytest.mark.parametrize("duplicates", test_data["duplicates"])
-def duplicates_have_same_key_test(duplicates):
+def test_duplicates(duplicates):
   logging.debug(str(duplicates))
   keys = set(map(get_approx_deduplicating_key, duplicates))
-  assert len(keys) == 1, str(duplicates)
+  assert len(keys) == 1, str(duplicates) + "-----" + str(keys)
+
+@pytest.mark.parametrize("non_duplicates", test_data["non_duplicates"])
+def test_non_duplicates(non_duplicates):
+  logging.debug(str(non_duplicates))
+  keys = set(map(get_approx_deduplicating_key, non_duplicates))
+  assert len(keys) > 1, str(non_duplicates) + "-----" + str(keys)
+

692aca3ec52b89e7b0d90b47400d6d4cbecdb39e
vvasuki <vishvas.vasuki@gmail.com>
2018-09-02 02:21:30 +0530

    Cleanup xsanscript

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='1.6.4',
+  version='1.6.5',
 
   description='Transliteration tools to convert text in one indic script encoding to another',
   long_description=long_description,
@@ -69,8 +69,8 @@ setup(
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 
   # What does your project relate to?

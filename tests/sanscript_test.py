448cb1244eeb0aa6b34061e8fffbef3bc5718faf
vvasuki <vishvas.vasuki@gmail.com>
2018-10-04 23:03:18 -0700

    Fix optitrans

--- tests/sanscript_test.py
+++ tests/sanscript_test.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 import logging
 from unittest import TestCase
 
-from indic_transliteration import sanscript as S
+from indic_transliteration import sanscript
 
 # Remove all handlers associated with the root logger object.
 for handler in logging.root.handlers[:]:
@@ -24,7 +24,7 @@ logging.basicConfig(
 )
 
 DATA = {
-  S.BENGALI: {
+  sanscript.BENGALI: {
     'vowels': 'অ আ ই ঈ উ ঊ ঋ ৠ ঌ ৡ এ ঐ ও ঔ',
     'marks': 'ক খা গি ঘী ঙু চূ ছৃ জৄ ঝৢ ঞৣ টে ঠৈ ডো ঢৌ ণং তঃ থ্',
     'consonants': """ক খ গ ঘ ঙ চ ছ জ ঝ ঞ ট ঠ ড ঢ ণ ত থ দ ধ ন প ফ ব ভ ম
@@ -34,7 +34,7 @@ DATA = {
     'naraIti': 'নর ইতি',
     'sentence': 'ধর্মক্ষেত্রে কুরুক্ষেত্রে সমবেতা যুযুত্সবঃ ।'
   },
-  S.DEVANAGARI: {
+  sanscript.DEVANAGARI: {
     'vowels': 'अ आ इ ई उ ऊ ऋ ॠ ऌ ॡ ए ऐ ओ औ',
     'marks': 'क खा गि घी ङु चू छृ जॄ झॢ ञॣ टे ठै डो ढौ णं तः थ्',
     'consonants': """क ख ग घ ङ च छ ज झ ञ ट ठ ड ढ ण त थ द ध न प फ ब भ म
@@ -44,7 +44,7 @@ DATA = {
     'naraIti': 'नर इति',
     'sentence': 'धर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः ।'
   },
-  S.GUJARATI: {
+  sanscript.GUJARATI: {
     'vowels': 'અ આ ઇ ઈ ઉ ઊ ઋ ૠ ઌ ૡ એ ઐ ઓ ઔ',
     'marks': 'ક ખા ગિ ઘી ઙુ ચૂ છૃ જૄ ઝૢ ઞૣ ટે ઠૈ ડો ઢૌ ણં તઃ થ્',
     'consonants': """ક ખ ગ ઘ ઙ ચ છ જ ઝ ઞ ટ ઠ ડ ઢ ણ ત થ દ ધ ન પ ફ બ ભ મ
@@ -54,7 +54,7 @@ DATA = {
     'naraIti': 'નર ઇતિ',
     'sentence': 'ધર્મક્ષેત્રે કુરુક્ષેત્રે સમવેતા યુયુત્સવઃ ૤'
   },
-  S.HK: {
+  sanscript.HK: {
     'vowels': 'a A i I u U R RR lR lRR e ai o au',
     'marks': """ka khA gi ghI Gu cU chR jRR jhlR JlRR Te Thai Do Dhau
                     NaM taH th""",
@@ -66,7 +66,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmakSetre kurukSetre samavetA yuyutsavaH |'
   },
-  S.ITRANS: {
+  sanscript.ITRANS: {
     'vowels': 'a A i I u U RRi RRI LLi LLI e ai o au',
     'marks': """ka khA gi ghI ~Nu chU ChRRi jRRI jhLLi ~nLLI Te Thai Do Dhau
                     NaM taH th""",
@@ -78,7 +78,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmakShetre kurukShetre samavetA yuyutsavaH |'
   },
-  S.OPTITRANS: {
+  sanscript.OPTITRANS: {
     'vowels': 'a A i I u U R RR LLi LLI e ai o au',
     'marks': """ka khA gi ghI ~Nu chU ChR jRR jhLLi ~nLLI Te Thai Do Dhau
                     NaM taH th""",
@@ -90,7 +90,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmakShetre kurukShetre samavetA yuyutsavaH |'
   },
-  S.VELTHUIS: {
+  sanscript.VELTHUIS: {
     'vowels': 'a aa i ii u uu .r .rr .l .ll e ai o au',
     'marks': """ka khaa gi ghii "nu cuu ch.r j.rr jh.l ~n.ll .te .thai .do .dhau
                     .na.m ta.h th""",
@@ -102,7 +102,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmak.setre kuruk.setre samavetaa yuyutsava.h |'
   },
-  S.IAST: {
+  sanscript.IAST: {
     'vowels': 'a ā i ī u ū ṛ ṝ ḷ ḹ e ai o au',
     'marks': 'ka khā gi ghī ṅu cū chṛ jṝ jhḷ ñḹ ṭe ṭhai ḍo ḍhau ṇaṃ taḥ th',
     'consonants': """ka kha ga gha ṅa ca cha ja jha ña ṭa ṭha ḍa ḍha ṇa
@@ -113,7 +113,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmakṣetre kurukṣetre samavetā yuyutsavaḥ ।'
   },
-  S.KOLKATA: {
+  sanscript.KOLKATA: {
     'vowels': 'a ā i ī u ū ṛ ṝ ḷ ḹ ē ai ō au',
     'marks': 'ka khā gi ghī ṅu cū chṛ jṝ jhḷ ñḹ ṭē ṭhai ḍō ḍhau ṇaṃ taḥ th',
     'consonants': """ka kha ga gha ṅa ca cha ja jha ña ṭa ṭha ḍa ḍha ṇa
@@ -124,7 +124,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'dharmakṣētrē kurukṣētrē samavētā yuyutsavaḥ ।'
   },
-  S.KANNADA: {
+  sanscript.KANNADA: {
     'vowels': 'ಅ ಆ ಇ ಈ ಉ ಊ ಋ ೠ ಌ ೡ ಏ ಐ ಓ ಔ',
     'marks': 'ಕ ಖಾ ಗಿ ಘೀ ಙು ಚೂ ಛೃ ಜೄ ಝೢ ಞೣ ಟೇ ಠೈ ಡೋ ಢೌ ಣಂ ತಃ ಥ್',
     'consonants': """ಕ ಖ ಗ ಘ ಙ ಚ ಛ ಜ ಝ ಞ ಟ ಠ ಡ ಢ ಣ ತ ಥ ದ ಧ ನ ಪ ಫ ಬ ಭ ಮ
@@ -134,7 +134,7 @@ DATA = {
     'naraIti': 'ನರ ಇತಿ',
     'sentence': 'ಧರ್ಮಕ್ಷೇತ್ರೇ ಕುರುಕ್ಷೇತ್ರೇ ಸಮವೇತಾ ಯುಯುತ್ಸವಃ ।'
   },
-  S.MALAYALAM: {
+  sanscript.MALAYALAM: {
     'vowels': 'അ ആ ഇ ഈ ഉ ഊ ഋ ൠ ഌ ൡ ഏ ഐ ഓ ഔ',
     'marks': 'ക ഖാ ഗി ഘീ ങു ചൂ ഛൃ ജൄ ഝൢ ഞൣ ടേ ഠൈ ഡോ ഢൌ ണം തഃ ഥ്',
     'consonants': """ക ഖ ഗ ഘ ങ ച ഛ ജ ഝ ഞ ട ഠ ഡ ഢ ണ ത ഥ ദ ധ ന പ ഫ ബ ഭ മ
@@ -144,7 +144,7 @@ DATA = {
     'naraIti': 'നര ഇതി',
     'sentence': 'ധര്മക്ഷേത്രേ കുരുക്ഷേത്രേ സമവേതാ യുയുത്സവഃ ।'
   },
-  S.SLP1: {
+  sanscript.SLP1: {
     'vowels': 'a A i I u U f F x X e E o O',
     'marks': 'ka KA gi GI Nu cU Cf jF Jx YX we WE qo QO RaM taH T',
     'consonants': """ka Ka ga Ga Na ca Ca ja Ja Ya wa Wa qa Qa Ra
@@ -155,7 +155,7 @@ DATA = {
     'naraIti': 'nara iti',
     'sentence': 'Darmakzetre kurukzetre samavetA yuyutsavaH .'
   },
-  S.WX: {
+  sanscript.WX: {
     'vowels': 'a A i I u U q Q L ḹ e E o O',
     'marks': 'ka KA gi GI fu cU Cq jQ JL Fḹ te TE do DO NaM waH W',
     'consonants': """ka Ka ga Ga fa ca Ca ja Ja Fa ta Ta da Da Na
@@ -166,7 +166,7 @@ DATA = {
     'naraIti': 'nara iwi',
     'sentence': 'XarmakRewre kurukRewre samavewA yuyuwsavaH .'
   },
-  S.TELUGU: {
+  sanscript.TELUGU: {
     'vowels': 'అ ఆ ఇ ఈ ఉ ఊ ఋ ౠ ఌ ౡ ఏ ఐ ఓ ఔ',
     'marks': 'క ఖా గి ఘీ ఙు చూ ఛృ జౄ ఝౢ ఞౣ టే ఠై డో ఢౌ ణం తః థ్',
     'consonants': """క ఖ గ ఘ ఙ చ ఛ జ ఝ ఞ ట ఠ డ ఢ ణ త థ ద ధ న ప ఫ బ భ మ
@@ -182,8 +182,8 @@ DATA = {
 class SanscriptTestCase(TestCase):
   """Ordinary :class:`~unittest.TestCase` with some helper data."""
 
-  roman = {S.HK, S.IAST, S.SLP1, S.ITRANS, S.WX, S.KOLKATA, S.VELTHUIS, S.OPTITRANS}
-  brahmic = {x for x in S.SCHEMES} - roman
+  roman = {sanscript.HK, sanscript.IAST, sanscript.SLP1, sanscript.ITRANS, sanscript.WX, sanscript.KOLKATA, sanscript.VELTHUIS, sanscript.OPTITRANS}
+  brahmic = {x for x in sanscript.SCHEMES} - roman
 
   def compare_all(self, _from, _to):
     """Compare all data for `_from` and `_to`"""
@@ -196,9 +196,9 @@ class SanscriptTestCase(TestCase):
     source = DATA[_from][group]
 
     # Regularize whitespace
-    actual = ' '.join(S.transliterate(source, _from, _to).split())
+    actual = ' '.join(sanscript.transliterate(source, _from, _to).split())
     expected = ' '.join(DATA[_to][group].split())
-    self.assertEqual(expected, actual)
+    self.assertEqual(expected, actual, "Failure ahoy: %s to %s: expected %s, got %s" % (_from, _to, expected, actual))
 
 
 class GeneralTestCase(SanscriptTestCase):
@@ -206,17 +206,17 @@ class GeneralTestCase(SanscriptTestCase):
 
   def test_membership(self):
     """Test that a scheme is roman iff `is_roman`"""
-    for name in S.SCHEMES:
-      self.assertEqual(S.SCHEMES[name].is_roman, name in self.roman)
+    for name in sanscript.SCHEMES:
+      self.assertEqual(sanscript.SCHEMES[name].is_roman, name in self.roman)
 
   def test_correspondence(self):
     """Test that schemes correspond to a subset of Devanagari.
 
     Since Devanagari is the most comprehensive scheme available, every
     scheme corresponds to a subset of Devanagari."""
-    dev = S.SCHEMES[S.DEVANAGARI]
+    dev = sanscript.SCHEMES[sanscript.DEVANAGARI]
     groups = set(dev.keys())
-    for name, scheme in S.SCHEMES.items():
+    for name, scheme in sanscript.SCHEMES.items():
       for group in scheme:
         logging.debug(name)
         logging.debug(group)
@@ -245,9 +245,9 @@ class ItransSynonymsTestCase(SanscriptTestCase):
 
   def test_devanaagarii_equivalence(self):
     """Test all synonmous transliterations."""
-    logging.info(S.transliterate("rAmo gUDhaM vaktI~Ngitaj~naH kShetre", S.ITRANS, S.DEVANAGARI))
-    self.assertEqual(S.transliterate("rAmo gUDhaM vaktI~Ngitaj~naH kShetre", S.ITRANS, S.DEVANAGARI),
-                     S.transliterate("raamo guuDhaM vaktii~NgitaGYaH xetre", S.ITRANS, S.DEVANAGARI))
+    logging.info(sanscript.transliterate("rAmo gUDhaM vaktI~Ngitaj~naH kShetre", sanscript.ITRANS, sanscript.DEVANAGARI))
+    self.assertEqual(sanscript.transliterate("rAmo gUDhaM vaktI~Ngitaj~naH kShetre", sanscript.ITRANS, sanscript.DEVANAGARI),
+                     sanscript.transliterate("raamo guuDhaM vaktii~NgitaGYaH xetre", sanscript.ITRANS, sanscript.DEVANAGARI))
 
 
 class BrahmicTestCase(SanscriptTestCase):
@@ -255,13 +255,13 @@ class BrahmicTestCase(SanscriptTestCase):
 
   def test_to_roman(self):
     """Test Brahmic to roman."""
-    _from = S.DEVANAGARI
+    _from = sanscript.DEVANAGARI
     for _to in self.roman:
       self.compare_all(_from, _to)
 
   def test_to_brahmic(self):
     """Test Brahmic to Brahmic."""
-    _from = S.DEVANAGARI
+    _from = sanscript.DEVANAGARI
     for _to in self.brahmic:
       self.compare_all(_from, _to)
 
@@ -270,13 +270,13 @@ class ToggleTestCase(SanscriptTestCase):
   """Test suspending then resuming transliteration."""
 
   def t_helper(self, _from, _to):
-    def func(input, output):
-      self.assertEqual(output, S.transliterate(input, _from, _to)), "_from: %s, _to: %s, _input: %s" % (_from, _to, input)
+    def func(data, output):
+      self.assertEqual(output, sanscript.transliterate(data, _from, _to)), "_from: %s, _to: %s, _input: %s" % (_from, _to, data)
 
     return func
 
   def test_toggle(self):
-    f = self.t_helper(S.HK, S.DEVANAGARI)
+    f = self.t_helper(sanscript.HK, sanscript.DEVANAGARI)
     f('akSa##kSa##ra', 'अक्षkSaर')
     f('##akSa##kSa##ra', 'akSaक्षra')
     f('akSa##ra##', 'अक्षra')
@@ -286,9 +286,9 @@ class ToggleTestCase(SanscriptTestCase):
     f('a#kSara', 'अ#क्षर')
 
   def test_suspend(self):
-    f = self.t_helper(S.HK, S.DEVANAGARI)
+    f = self.t_helper(sanscript.HK, sanscript.DEVANAGARI)
     f('<p>nara iti</p>', '<p>नर इति</p>')
 
   def test_suspend_and_toggle(self):
-    f = self.t_helper(S.HK, S.DEVANAGARI)
+    f = self.t_helper(sanscript.HK, sanscript.DEVANAGARI)
     f('<p>##na##ra## iti</p>', '<p>naर iti</p>')

60f579d9b0b0fbd08a928500921ac30c7b0baff3
vvasuki <vishvas.vasuki@gmail.com>
2018-12-25 15:11:41 -0800

    Add a tech hindi converter.

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=['regex', 'backports.functools_lru_cache'],
+  install_requires=['regex', 'backports.functools_lru_cache', 'splinter'],
 
   # List additional groups of dependencies here (e.g. development
   # dependencies). You can install these using the following syntax,

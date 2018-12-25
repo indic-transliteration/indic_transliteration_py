a6c4489f74a364040baea8e21f252319a61bbd55
vvasuki <vishvas.vasuki@gmail.com>
2018-12-25 14:42:30 -0800

    Add a tech hindi converter.

---
+++ indic_transliteration/tech_hindi/__init__.py
@@ -0,0 +1,19 @@
+import os.path
+
+from splinter.browser import Browser
+
+
+class DVTTVedicConverter(object):
+    def __init__(self):
+        # We presume that you've installed chrome driver as per https://splinter.readthedocs.io/en/latest/drivers/chrome.html .
+        self.browser = Browser('chrome')
+        self.browser.visit('file://' + os.path.join(os.path.dirname(__file__), "data", 'DV-TTVedicNormal ==_ यूनिकोड परिवर्तित्र.html'))
+        
+    def convert(self, text):
+        input_box = self.browser.find_by_id("legacy_text")
+        convert_button = self.browser.find_by_name("converter")
+        input_box.fill(text)
+        convert_button.click()
+        output_box = self.browser.find_by_id("unicode_text")
+        return output_box.value
+
---
+++ tests/tech_hindi_test.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+import logging
+
+from indic_transliteration import tech_hindi
+
+# Remove all handlers associated with the root logger object.
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(filename)s:%(lineno)d %(message)s"
+)
+
+def test_dvt_vedic():
+    converter = tech_hindi.DVTTVedicConverter()
+    text_in = "    +<=hÉÂ *1* +EòÉ®úÉä Ê´É´ÉÞiÉ ={ÉÊnù¹]õ& |ÉÉÊGòªÉÉnù¶ÉÉªÉÉÆ SÉäiªÉjÉ \"+ +' (ºÉÚ.8-4-68)  "
+    output = converter.convert(text_in)
+    expected = "    अइउण् ।1। अकारो विवृत उपदिष्टः प्राक्रियादशायां चेत्यत्र \"अ अ\" (सू.8-4-68)  "
+    assert output == expected, u'%s == %s (%s in %s)' % (output, expected, text_in, converter.__class__)

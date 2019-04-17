bc43188cfae1702082ff2906a831ab6351969a5e
vvasuki <vishvas.vasuki@gmail.com>
2019-04-17 11:18:59 -0700

    Content update

---
+++ indic_transliteration/sanscript/schemes/brahmic/northern.py
@@ -0,0 +1,85 @@
+from indic_transliteration.sanscript.schemes.brahmic import BrahmiScheme, s, DEVANAGARI, GUJARATI, GURMUKHI
+
+
+class DevanagariScheme(BrahmiScheme):
+    def __init__(self):
+        super(DevanagariScheme, self).__init__({
+            'vowels': s("""अ आ इ ई उ ऊ ऋ ॠ ऌ ॡ ए ऐ ओ औ ऎ ऒ"""),
+            'marks': s("""ा ि ी ु ू ृ ॄ ॢ ॣ े ै ो ौ ॆ ॊ"""),
+            'virama': s('्'),
+            'yogavaahas': s('ं ः ँ'),
+            'consonants': s("""
+                            क ख ग घ ङ
+                            च छ ज झ ञ
+                            ट ठ ड ढ ण
+                            त थ द ध न
+                            प फ ब भ म
+                            य र ल व
+                            श ष स ह
+                            ळ क्ष ज्ञ
+                            """)
+                          + s("""ऩ ऱ ऴ क़ ख़ ग़ ज़ ड़ ढ़ फ़ य़"""),
+            'symbols': s("""
+                       ॐ ऽ । ॥
+                       ० १ २ ३ ४ ५ ६ ७ ८ ९
+                       """)
+        }, name=DEVANAGARI)
+
+    @classmethod
+    def fix_lazy_visarga(cls, data_in):
+        data_out = data_in
+        import regex
+        data_out = regex.sub(r'ः( *)([क-ङ])', r'ᳵ\1\2',   data_out)
+        data_out = regex.sub(r'ः( *)([प-म])', r'ᳶ\1\2',   data_out)
+        return data_out
+
+
+class GujaratiScheme(BrahmiScheme):
+    def __init__(self):
+        super(GujaratiScheme, self).__init__({
+            'vowels': s("""અ આ ઇ ઈ ઉ ઊ ઋ ૠ ઌ ૡ એ ઐ ઓ ઔ"""),
+            'marks': s("""ા િ ી ુ ૂ ૃ ૄ ૢ ૣ ે ૈ ો ૌ"""),
+            'virama': s('્'),
+            'yogavaahas': s('ં ઃ ઁ'),
+            'consonants': s("""
+                            ક ખ ગ ઘ ઙ
+                            ચ છ જ ઝ ઞ
+                            ટ ઠ ડ ઢ ણ
+                            ત થ દ ધ ન
+                            પ ફ બ ભ મ
+                            ય ર લ વ
+                            શ ષ સ હ
+                            ળ ક્ષ જ્ઞ
+                            """)
+                          + s("""ન઼ ર઼ ળ઼ ક઼ ખ઼ ગ઼ જ઼ ડ઼ ઢ઼ ફ઼ ય઼"""),
+            'symbols': s("""
+                       ૐ ઽ ૤। ॥૥
+                       ૦ ૧ ૨ ૩ ૪ ૫ ૬ ૭ ૮ ૯
+                       """)
+        }, name=GUJARATI)
+
+
+class GurmukhiScheme(BrahmiScheme):
+    def __init__(self):
+        super(GurmukhiScheme, self).__init__({
+            'vowels': s("""ਅ ਆ ਇ ਈ ਉ ਊ ऋ ॠ ऌ ॡ ਏ ਐ ਓ ਔ"""),
+            'marks': ['ਾ', 'ਿ', 'ੀ', 'ੁ', 'ੂ', '', '',
+                      '', '', 'ੇ', 'ੈ', 'ੋ', 'ੌ'],
+            'virama': s('੍'),
+            'yogavaahas': s('ਂ ਃ ਁ'),
+            'consonants': s("""
+                            ਕ ਖ ਗ ਘ ਙ
+                            ਚ ਛ ਜ ਝ ਞ
+                            ਟ ਠ ਡ ਢ ਣ
+                            ਤ ਥ ਦ ਧ ਨ
+                            ਪ ਫ ਬ ਭ ਮ
+                            ਯ ਰ ਲ ਵ
+                            ਸ਼ ਸ਼਼ ਸ ਹ
+                            ਲ਼ ਕ੍ਸ਼ ਜ੍ਞ
+                            """)
+                          + s("""ਨ਼ ਰ਼ ਲ਼਼ ਕ਼ ਖ਼ ਗ਼ ਜ਼ ੜ ਢ਼ ਫ਼ ਯ਼"""),
+            'symbols': s("""
+                       ੴ ऽ । ॥
+                       ੦ ੧ ੨ ੩ ੪ ੫ ੬ ੭ ੮ ੯
+                       """)
+        }, name=GURMUKHI)

1451f226a5b093a33ac70e12a09f11de3d4b9190
vvasuki <vishvas.vasuki@gmail.com>
2020-04-12 07:46:11 +0530

    Switch to selenium. Might possibly fix https://travis-ci.org/github/sanskrit-coders/indic_transliteration

--- indic_transliteration/font_converter/tech_hindi/__init__.py
+++ indic_transliteration/font_converter/tech_hindi/__init__.py
@@ -1,19 +1,33 @@
+import logging
 import os.path
 
-from splinter.browser import Browser
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.remote.remote_connection import LOGGER
+
+LOGGER.setLevel(logging.WARNING)
+from urllib3.connectionpool import log as urllibLogger
+urllibLogger.setLevel(logging.WARNING)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
+
+opts = options.Options()
+opts.headless = True
 
 
 class DVTTVedicConverter(object):
     def __init__(self):
         # We presume that you've installed chrome driver as per https://splinter.readthedocs.io/en/latest/drivers/chrome.html .
-        self.browser = Browser('chrome', headless=True)
-        self.browser.visit('file://' + os.path.join(os.path.dirname(__file__), "data", 'DV-TTVedicNormal ==_ यूनिकोड परिवर्तित्र.html'))
-        
+        self.browser = webdriver.Chrome(options=opts)
+        self.browser.get('file://' + os.path.join(os.path.dirname(__file__), "data", 'DV-TTVedicNormal ==_ यूनिकोड परिवर्तित्र.html'))
+
     def convert(self, text):
-        input_box = self.browser.find_by_id("legacy_text")
-        convert_button = self.browser.find_by_name("converter")
-        input_box.fill(text)
+        input_box = self.browser.find_element_by_id("legacy_text")
+        convert_button = self.browser.find_element_by_name("converter")
+        input_box.send_keys(text)
         convert_button.click()
-        output_box = self.browser.find_by_id("unicode_text")
-        return output_box.value
+        output_box = self.browser.find_element_by_id("unicode_text")
+        return output_box.get_attribute("value")
 
--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=['regex', 'backports.functools_lru_cache', 'splinter'],
+  install_requires=['regex', 'backports.functools_lru_cache', 'selenium'],
 
   # List additional groups of dependencies here (e.g. development
   # dependencies). You can install these using the following syntax,

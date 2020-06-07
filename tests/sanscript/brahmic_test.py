32536b36471b2c93f3bf7a4040ae228f378688a3
vvasuki <vishvas.vasuki@gmail.com>
2020-06-07 21:08:03 +0530

    cu

--- tests/sanscript/brahmic_test.py
+++ tests/sanscript/brahmic_test.py
@@ -4,6 +4,7 @@ from indic_transliteration import sanscript
 
 def test_fix_lazy_anusvaara_devanagari():
     assert sanscript.SCHEMES[sanscript.DEVANAGARI].fix_lazy_anusvaara("तं जित्वा") == "तञ् जित्वा"
+    assert sanscript.SCHEMES[sanscript.DEVANAGARI].fix_lazy_anusvaara("जगइ") == "जगइ"
 
 def test_fix_lazy_visarga():
     assert sanscript.SCHEMES[sanscript.DEVANAGARI].fix_lazy_visarga("अन्तः पश्य") == "अन्तᳶ पश्य"

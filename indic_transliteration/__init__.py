9fb870095645fbffc2c6a606219074b7d9c8a710
vvasuki <vishvas.vasuki@gmail.com>
2020-10-02 18:01:35 +0530

    CU

--- indic_transliteration/__init__.py
+++ indic_transliteration/__init__.py
@@ -1,3 +1,7 @@
 """
 :py:mod:`~indic_transliteration.sanscript` is the most popular submodule here.
 """
+import json
+import os
+
+language_code_to_script = json.load(os.path.join(os.dirname(__file__), "language_code_to_script.json"))

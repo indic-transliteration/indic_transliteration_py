af223f06e235eeacba019be12bcf1c01b67ec031
vvasuki <vishvas.vasuki@gmail.com>
2021-07-29 11:00:21 +0530

    New migration

--- indic_transliteration/sanscript/schemes/migrator.py
+++ indic_transliteration/sanscript/schemes/migrator.py
@@ -17,14 +17,21 @@ SCHEMES = {}
 
 def migrate(source_dir, dest_dir):
   old_files = glob.glob(os.path.join(source_dir, "**/*.json"))
-  for old_path in old_files:
-    logging.info(old_path)
-    with codecs.open(old_path, "r", 'utf-8') as file_in:
-      scheme = json.load(file_in)
-    new_path = old_path.replace(source_dir, dest_dir).replace(".json", ".toml")
-    os.makedirs(name=os.path.dirname(new_path), exist_ok=True)
-    with codecs.open(new_path, "w", 'utf-8') as file_in:
-      toml.dump(scheme, file_in)
+  with codecs.open(os.path.join(source_dir, "brahmic/devanagari.json"), "r", 'utf-8') as file_in:
+    devanagari_scheme = json.load(file_in)
+    for old_path in old_files:
+      new_path = old_path.replace(source_dir, dest_dir).replace(".json", ".toml")
+      logging.info("%s → %s", os.path.basename(old_path), os.path.basename(new_path))
+      os.makedirs(name=os.path.dirname(new_path), exist_ok=True)
+      with codecs.open(old_path, "r", 'utf-8') as file_in, codecs.open(new_path, "w", 'utf-8') as file_out:
+        scheme = json.load(file_in)
+        new_scheme = {}
+        for key in scheme:
+          if key in devanagari_scheme and not key.startswith("alternate"):
+            new_scheme[key] = dict(zip(devanagari_scheme[key], scheme[key]))
+          else:
+            new_scheme[key] = scheme[key]
+        toml.dump(new_scheme, file_out)
 
 
 if __name__ == '__main__':

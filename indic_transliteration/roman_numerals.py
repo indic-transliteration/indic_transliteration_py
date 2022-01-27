358038ee01b76f1ae086cdb8ce12323bc14a5982
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-01-27 23:36:19 +0530

    CU

---
+++ indic_transliteration/roman_numerals.py
@@ -0,0 +1,154 @@
+"""
+Library to convert to and from Roman numerals in various encodings
+
+Placed here till https://github.com/jambonrose/roman-numerals/pull/64 is accepted.
+"""
+
+from re import (
+  VERBOSE, compile as re_compile, fullmatch, match, sub as substitute,)
+
+# Operation Modes
+STANDARD = 1
+LOWERCASE = 2
+
+ROMAN_NUMERAL_TABLE = [
+  (1000, 'Ⅿ'),
+  (900, 'ⅭⅯ'),
+  (500, 'Ⅾ'),
+  (400, 'ⅭⅮ'),
+  (100, 'Ⅽ'),
+  (90, 'ⅩⅭ'),
+  (50, 'Ⅼ'),
+  (40, 'ⅩⅬ'),
+  (10, 'Ⅹ'),
+  (9, 'Ⅸ'),
+  (5, 'Ⅴ'),
+  (4, 'Ⅳ'),
+  (1, 'Ⅰ'),
+]
+
+SHORTENINGS = [
+  ('ⅩⅠⅠ', 'Ⅻ'),
+  ('ⅩⅠ', 'Ⅺ'),
+  ('ⅠⅩ', 'Ⅸ'),
+  ('ⅤⅠⅠⅠ', 'Ⅷ'),
+  ('ⅤⅠⅠ', 'Ⅶ'),
+  ('ⅤⅠ', 'Ⅵ'),
+  ('ⅠⅠⅠ', 'Ⅲ'),
+  ('ⅠⅠ', 'Ⅱ'),
+]
+STANDARD_TRANS = 'ⅯⅮⅭⅬⅫⅪⅩⅨⅧⅦⅥⅤⅣⅢⅡⅠ'
+LOWERCASE_TRANS = 'ⅿⅾⅽⅼⅻⅺⅹⅸⅷⅶⅵⅴⅳⅲⅱⅰ'
+
+
+def convert_to_numeral(decimal_integer: int, mode: int = STANDARD) -> str:
+  """Convert a decimal integer to a Roman numeral"""
+  if (not isinstance(decimal_integer, int)
+      or isinstance(decimal_integer, bool)):
+    raise TypeError("decimal_integer must be of type int")
+  if (not isinstance(mode, int)
+      or isinstance(mode, bool)
+      or mode not in [LOWERCASE, STANDARD]):
+    raise ValueError(
+      "mode must be "
+      "roman_numerals.STANDARD "
+      "or roman_numerals.LOWERCASE "
+    )
+  return_list = []
+  remainder = decimal_integer
+  for integer, numeral in ROMAN_NUMERAL_TABLE:
+    repetitions, remainder = divmod(remainder, integer)
+    return_list.append(numeral * repetitions)
+  numeral_string = ''.join(return_list)
+
+  numeral_string = use_shortenings(numeral_string)
+
+  if mode == LOWERCASE:
+    trans_to_lowercase = str.maketrans(STANDARD_TRANS, LOWERCASE_TRANS)
+    numeral_string = numeral_string.translate(trans_to_lowercase)
+  return numeral_string
+
+
+def use_shortenings(numeral_string):
+  for full_string, shortening in SHORTENINGS:
+    numeral_string = substitute(
+      r'%s$' % full_string,
+      shortening,
+      numeral_string,
+    )
+  return numeral_string
+
+
+NUMERAL_PATTERN = re_compile(
+  """
+  Ⅿ*                # thousands
+  (ⅭⅯ|ⅭⅮ|Ⅾ?Ⅽ{0,3})  # hundreds - ⅭⅯ (900), ⅭⅮ (400), ⅮⅭⅭⅭ (800), ⅭⅭⅭ (300)
+  (ⅩⅭ|ⅩⅬ|Ⅼ?Ⅹ{0,3})  # tens - ⅩⅭ (90), ⅩⅬ (40), ⅬⅩⅩⅩ (80), ⅩⅩⅩ (30)
+  (Ⅸ|Ⅳ|Ⅴ?Ⅰ{0,3})   # ones - Ⅸ (9), Ⅳ (4), ⅤⅠⅠⅠ (8), ⅠⅠⅠ (3)
+  """,
+  VERBOSE
+)
+
+
+def convert_to_integer(roman_numeral: str) -> int:
+  """Convert a Roman numeral to a decimal integer"""
+  if not isinstance(roman_numeral, str):
+    raise TypeError("decimal_integer must be of type int")
+  if roman_numeral == '':
+    raise ValueError("roman_numeral cannot be an empty string")
+
+  # ensure all characters are in the standard/uppercase set
+  trans_to_uppercase = str.maketrans(LOWERCASE_TRANS, STANDARD_TRANS)
+  # named partial_numeral because it will be shortened in loop below
+  partial_numeral = roman_numeral.translate(trans_to_uppercase)
+
+  partial_numeral = remove_shortenings(partial_numeral)
+
+  if not fullmatch(NUMERAL_PATTERN, partial_numeral):
+    raise ValueError(
+      "the string %s is not a valid numeral" % roman_numeral
+    )
+
+  # convert uppercase roman numerals to integer
+  return_value = 0
+  for integer, numeral in ROMAN_NUMERAL_TABLE:
+    pattern_match = match(r'^(%s)+' % numeral, partial_numeral)
+    if pattern_match:
+      chars_matched = len(pattern_match.group())
+      numerals_matched = chars_matched // len(numeral)
+      return_value += numerals_matched * integer
+      partial_numeral = partial_numeral[chars_matched:]
+  return return_value
+
+
+def remove_shortenings(partial_numeral):
+  # remove Unicode shortenings in favor of chars in conversion table
+  for full_string, shortening in SHORTENINGS:
+    partial_numeral = substitute(
+      r'%s$' % shortening,
+      full_string,
+      partial_numeral,
+    )
+  return partial_numeral
+
+
+roman_to_ascii_map = {"Ⅰ": "I", "Ⅴ": "V", "Ⅹ": "X", "Ⅼ": "L", "Ⅽ": "C", "Ⅾ": "D", "Ⅿ": "M"}
+
+
+def roman_to_ascii(numeral_string):
+  numeral_string = remove_shortenings(value=numeral_string)
+  numeral_string.replace('Ⅳ', 'ⅠⅤ')
+  for rom, asc in roman_to_ascii_map.items():
+    numeral_string = numeral_string.replace(rom, asc)
+  return numeral_string
+
+
+
+def ascii_to_roman(numeral_string):
+  for rom, asc in roman_to_ascii_map.items():
+    numeral_string = numeral_string.replace(asc, rom)
+  numeral_string = use_shortenings(numeral_string)
+  numeral_string.replace('ⅠⅤ', 'Ⅳ')
+  return numeral_string
+
+
---
+++ tests/roman_numerals/test_to_integer.py
@@ -0,0 +1,62 @@
+"""
+Test conversion from integer to Roman numeral
+"""
+
+from typing import Any
+
+import pytest
+
+from indic_transliteration.roman_numerals import convert_to_integer
+
+from .parameters import LOWERCASE_PARAMETERS, STANDARD_PARAMETERS
+
+
+@pytest.mark.parametrize(
+  "expected_integer, roman_numeral",
+  LOWERCASE_PARAMETERS + STANDARD_PARAMETERS)
+def test_integer_conversion(
+    roman_numeral: str, expected_integer: int,
+) -> None:
+  """
+  Test conversion from integers to uppercase Unicode Roman numerals
+  """
+  assert convert_to_integer(roman_numeral) == expected_integer
+
+
+@pytest.mark.parametrize("non_strings", [
+  2,
+  5.0,
+  True,
+  set(),
+  {'hello': 4},
+  lambda: print('called!'),  # pragma: no cover
+])
+def test_invalid_types(non_strings: Any) -> None:
+  """
+  Ensure that passing in non-strings results in Type exceptions
+  """
+  with pytest.raises(TypeError):
+    convert_to_integer(non_strings)
+
+
+@pytest.mark.parametrize("invalid_numerals", [
+  '',
+  'ⅠⅯ',
+  'ⅰⅿ',
+  'ⅠⅮ',
+  'ⅰⅾ',
+  'ⅠⅭ',
+  'ⅰⅽ',
+  'ⅠⅬ',
+  'ⅰⅼ',
+  'ⅬⅯ',
+  'ⅼⅿ',
+  'ⅮⅯ',
+  'ⅾⅿ',
+])
+def test_invalid_numerals(invalid_numerals: Any) -> None:
+  """
+  Ensure that passing in invalid Roman numerals raises ValueErrors
+  """
+  with pytest.raises(ValueError):
+    convert_to_integer(invalid_numerals)

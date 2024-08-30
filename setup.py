c7adb611bd67cbfc0cfbb384be187a6d0f6e9458
vvasuki <vishvas.vasuki@gmail.com>
2024-08-30 17:28:15 +0530

    12

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='2.3.62',
+  version='2.3.63',
 
 
   description='Transliteration tools to convert text in one indic script encoding to another',

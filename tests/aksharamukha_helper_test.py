762b882769702897cc140e760573d938f5c29798
vvasuki <vishvas.vasuki@gmail.com>
2024-10-02 17:36:06 +0530

    ऩ

--- tests/aksharamukha_helper_test.py
+++ tests/aksharamukha_helper_test.py
@@ -3,12 +3,12 @@ import importlib
 import pytest
 import sys
 
-from indic_transliteration import aksharamukha_helper
 
 
 
 @pytest.mark.skipif(
   not importlib.util.find_spec("aksharamukha"), reason="requires the aksharamukha library")
 def test_transliterate_tamil():
+  from indic_transliteration import aksharamukha_helper
   assert aksharamukha_helper.transliterate_tamil("அற்று") == "अऱ्ऱु"
     

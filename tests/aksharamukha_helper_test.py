a5e66de9307d920d8d8c3f9f0c68735a8d5fe0dc
vvasuki <vishvas.vasuki@gmail.com>
2024-10-02 17:38:20 +0530

    ऩ

--- tests/aksharamukha_helper_test.py
+++ tests/aksharamukha_helper_test.py
@@ -4,11 +4,9 @@ import pytest
 import sys
 
 
+pytest.importorskip("aksharamukha", reason="The aksharamukha library is required for these tests.")
+from indic_transliteration import aksharamukha_helper
 
-
-@pytest.mark.skipif(
-  not importlib.util.find_spec("aksharamukha"), reason="requires the aksharamukha library")
 def test_transliterate_tamil():
-  from indic_transliteration import aksharamukha_helper
   assert aksharamukha_helper.transliterate_tamil("அற்று") == "अऱ्ऱु"
     
